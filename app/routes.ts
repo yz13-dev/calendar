1872c5f92a40ba22d47fa3f0cba619017768bc8d
YZ13 <yztheceo@yandex.ru>
2025-08-28 22:35:41 +0300

    add schedule page

--- app/routes.ts
+++ app/routes.ts
@@ -4,4 +4,5 @@ export default [
   index("routes/page.tsx"),
   route("login", "routes/login/page.tsx"),
   route("call/:uid/new", "routes/call/[uid]/new/page.tsx"),
+  route("/schedule", "routes/schedule/page.tsx"),
 ] satisfies RouteConfig;
---
+++ app/routes/schedule/page.tsx
@@ -0,0 +1,12 @@
+export default function () {
+  return (
+    <div className="w-full min-h-dvh flex *:py-3 gap-3">
+      <div className="w-1/4 pl-3">
+        <div className="w-full h-20 bg-secondary"></div>
+      </div>
+      <div className="w-3/4 pr-3">
+        <div className="w-full h-20 bg-secondary"></div>
+      </div>
+    </div>
+  )
+}
