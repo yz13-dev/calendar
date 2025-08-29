6f315f6d7a60c7b5ce781e0353dfab0a6d008d6f
YZ13 <yztheceo@yandex.ru>
2025-08-29 09:58:34 +0300

    update schedule page

--- app/routes/schedule/page.tsx
+++ app/routes/schedule/page.tsx
@@ -1,8 +1,30 @@
 export default function () {
   return (
     <div className="w-full min-h-dvh flex *:py-3 gap-3">
-      <div className="w-1/4 pl-3">
-        <div className="w-full h-20 bg-secondary"></div>
+      <div className="w-1/4 pl-3 space-y-3">
+
+        <div className="w-full flex flex-row">
+          <div className="size-20 flex flex-col items-center justify-center">
+            <span className="text-center text-muted-foreground">СРД</span>
+            <span className="text-4xl font-medium">20</span>
+          </div>
+          <div className="w-[calc(100%-80px)] space-y-3">
+            <div className="w-full bg-secondary h-20" />
+            <div className="w-full bg-secondary h-20" />
+            <div className="w-full bg-secondary h-20" />
+          </div>
+        </div>
+
+        <div className="w-full flex flex-row">
+          <div className="size-20 flex flex-col items-center justify-center">
+            <span className="text-center text-muted-foreground">ЧТГ</span>
+            <span className="text-4xl font-medium">21</span>
+          </div>
+          <div className="w-[calc(100%-80px)] space-y-3">
+            <div className="w-full bg-secondary h-20" />
+          </div>
+        </div>
+
       </div>
       <div className="w-3/4 pr-3">
         <div className="w-full h-20 bg-secondary"></div>
